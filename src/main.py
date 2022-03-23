956f55813f45dc480748211a26627ccba20c38ad
Zarif Aziz <zarif.aziz@faethm.ai>
2022-03-24 10:55:37 +1100

    using lighter model and better context

--- src/main.py
+++ src/main.py
@@ -1,20 +1,13 @@
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-# tokenizer = AutoTokenizer.from_pretrained('ktrapeznikov/albert-xlarge-v2-squad-v2')
-# model = AutoModelForQuestionAnswering.from_pretrained('ktrapeznikov/albert-xlarge-v2-squad-v2')
-# question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
-
-question_answerer = pipeline("question-answering")
+tokenizer = AutoTokenizer.from_pretrained('mrm8488/bert-small-finetuned-squadv2')
+model = AutoModelForQuestionAnswering.from_pretrained('mrm8488/bert-small-finetuned-squadv2')
+question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
 
 context=r"""
 Hello, my name is Zarif. I am 26 years old. I'm a software engineer and data scientist based in Sydney, 
 Australia focused on building AI-enabled software solutions for a wide range of problems. Currently working at Faethm.ai.
 """
-result = question_answerer(question="What is Zarif's name?", context=context)
-
-print(
-    f"Answer: '{result['answer']}', score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']}"
-)
 
 while True:
     try:
